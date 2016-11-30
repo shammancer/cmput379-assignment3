f18bc0e7cbd4df893c7f2d322888d37e626a0ee3
jessicaprt <jessicaprieto31@gmail.com>
2016-11-30 15:08:18 -0700

    i think heapsort is working

--- heapsort.c
+++ heapsort.c
@@ -3,13 +3,22 @@
 
 int lastleaf = 0;
 
-void sort(int * numbers) {
-	lastleaf = size(numbers) - 1;
+void heapify(int arr[], int node);
+
+void swap(int * arr, int i, int j) {
+	int buff = arr[i];
+	arr[i] = arr[j];
+	arr[j] = buff;
+}
+
+void sort(int numbers[], int size) {
+	printf("size (in sort): %i\n", size);
+	lastleaf = size - 1;
 	int j = 0;
 	for (j = lastleaf/2; j >= 0; j--) {
 		heapify(numbers,j);
 	} 
-	int i = 0
+	int i = 0;
 	for (i = lastleaf; i>0; i--) {
 		swap(numbers, 0, i);
 		lastleaf = lastleaf - 1;
@@ -17,9 +26,9 @@ void sort(int * numbers) {
 	}
 }
 
-void heapify(int * arr, int node) {
-	int left = node * 2 + 1;
-	int right = node * 2 + 2;
+void heapify(int arr[], int node) {
+	int left = (node*2) + 1;
+	int right = (node*2) + 2;
 	int greater = node;
 	if(left<=lastleaf) {
 		if(arr[left]>arr[greater]) {
@@ -35,19 +44,13 @@ void heapify(int * arr, int node) {
 	}
 }
 
-void swap(int * arr, int i, int j) {
-	int buff = arr[i];
-	arr[i] = arr[j];
-	arr[j] = buff;
-}
-
-void printArray(int * arr) {
+void printArray(int * arr, int size) {
 	int i = 0;
-	for(i=0; i<size(array); i++) {
+	for(i=0; i< size ; i++) {
 		if(i > 0) {
 			printf(", ");
 		}
-		print("%i", arr[i]);
+		printf("%i", arr[i]);
 	}
 	printf("\n");
 }
@@ -55,13 +58,15 @@ void printArray(int * arr) {
 int main() {
 	int numbers[10];
 	int i = 0;
-	for(int i = 0; i<10; i++) {
+	for(i = 0; i<10; i++) {
 		numbers[i] = rand() % 20;
 		printf("%i ", numbers[i]);
 	}
+
+	int arr_size = sizeof(numbers)/sizeof(int);
 	printf("\n");
 
-	sort(numbers);
+	sort(numbers, arr_size);
 
-	printArray(numbers);
+	printArray(numbers, arr_size);
 }
