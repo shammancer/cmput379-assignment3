b455cc0d5ac33f28f3216e11fd98c29c7ebe8036
Dannick Pomerleau <dannick@beta.priv.deuterium.ca>
2016-12-02 18:07:26 -0700

    Updated test...

--- test1.c
+++ test1.c
@@ -1,18 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <time.h>
+
 #include "simulation.h"
 
 void process () {
     /* This process function generates a number of integer */
     /* keys and sorts them using bubblesort.               */
     int N, i, j, k, t, min, f;
+    clock_t start, diff;
+    double msec;
+
     scanf ("%d", &N);
     printf ("Sorting %1d keys\n", N);
     init (128, 1000);
     /* Generate the sorting problem (just random numbers) */
+    start = clock();
     for (i = 0; i < N; i++) put (i, lrand48 ());
+    diff = clock() - start;
+    msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("T1 %f ms\n", msec);
+    fflush(stdout);
+
     /* Sort the numbers */
+    start = clock();
     for (i = 0; i < N-1; i++) {
         for (j = i+1, f = min = get (i), k = i; j < N; j++)
             if ((t = get (j)) < min) {
@@ -22,5 +34,15 @@ void process () {
             put (i, min);
             put (k, f);
     }
-    done ();
+    diff = clock() - start;
+    msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("T2 %f ms\n", msec);
+    fflush(stdout);
+
+    start = clock();
+    done();
+    diff = clock() - start;
+    msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("T3 %f ms\n", msec);
+    fflush(stdout);
 }
